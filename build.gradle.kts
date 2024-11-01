63ad4fb58cb66e054d313a114eb11774df6e1199
Václav Pokorný <vaclav.pokorny@gmail.com>
2024-11-01 03:20:58 +0100

    migrate to gradle, update java version, enable kotlin support

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    `java-library`
+    kotlin("jvm") version "2.0.21"
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+}
+
+group = "mil.nga"
+version = "3.0.1"
+description = "Tagged Image File Format"
+java.sourceCompatibility = JavaVersion.VERSION_21
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc>() {
+    options.encoding = "UTF-8"
+}
