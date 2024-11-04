42637db2756da0f31c378598628f8a6e3649e32c
Václav Pokorný <vaclav.pokorny@gmail.com>
2024-11-04 23:11:41 +0100

    update gradle and fix junit tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.11.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.3")
 }
 
 group = "mil.nga"
